d59abc54946a0c6e48344e6777e1231cd0a41e5e
tomhebcit <the18@my.bcit.ca>
2025-02-13 12:25:24 -0800

    test

--- Server2BackEnd/modules/patients.js
+++ Server2BackEnd/modules/patients.js
@@ -15,17 +15,17 @@ class Patients {
     this.userDB = new Database(false); // Use the default user
   }
 
-  async init() {
-    try {
-      await this.adminDB.connect(); // Ensure DB connection is established
-      await this.adminDB.ensureTableExists(); // Ensure table is created if it doesn't exist
-      await this.adminDB.close(); // Close the connection
-      await this.userDB.connect(); // Ensure DB connection is established
-      console.log('Patients class initialized successfully!');
-    } catch (err) {
-      console.error('Error during initialization:', err);
-    }
-  }
+  // async init() {
+  //   try {
+  //     await this.adminDB.connect(); // Ensure DB connection is established
+  //     await this.adminDB.ensureTableExists(); // Ensure table is created if it doesn't exist
+  //     await this.adminDB.close(); // Close the connection
+  //     await this.userDB.connect(); // Ensure DB connection is established
+  //     console.log('Patients class initialized successfully!');
+  //   } catch (err) {
+  //     console.error('Error during initialization:', err);
+  //   }
+  // }
 
   closeConnection() {
     this.userDB.close();
