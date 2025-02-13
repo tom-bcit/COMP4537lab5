07c35b73d17e8101f06089151bf6b69381340636
tomhebcit <the18@my.bcit.ca>
2025-02-13 12:51:11 -0800

    changed start() to be async so we can await patients.init

--- Server2BackEnd/server.js
+++ Server2BackEnd/server.js
@@ -1,32 +1,38 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const Patients = require('./modules/patients')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const Patients = require('./modules/patients');
 
 class Server {
 
     constructor() {
         this.patients = new Patients();
-        const messages = JSON.parse(fs.readFileSync('./lang/en/messages.json'))
-        this.notFound = messages.notFound
+        const messages = JSON.parse(fs.readFileSync('./lang/en/messages.json'));
+        this.notFound = messages.notFound;
         this.routes = [
             {pathRegex: /^\/api\/patients\/?$/, action: this.patients.handleRequest}
-        ]
+        ];
     }
 
-    start() {
-        http.createServer((req, res) => {
-            const urlParts = url.parse(req.url)
-            for (let i = 0; i < this.routes.length; i++) {
-                if (this.routes[i].pathRegex.test(urlParts.pathname.toLowerCase())) {
-                    return this.routes[i].action(req, res)
+    async start() {
+        try {
+            // Ensure the Patients class is initialized before starting the server
+            await this.patients.init();
+            http.createServer((req, res) => {
+                const urlParts = url.parse(req.url);
+                for (let i = 0; i < this.routes.length; i++) {
+                    if (this.routes[i].pathRegex.test(urlParts.pathname.toLowerCase())) {
+                        return this.routes[i].action(req, res);
+                    }
                 }
-            }
-            res.writeHead(404, { 'content-type': 'text/html' })
-            res.end(this.notFound)
-        }).listen(8089)
-        
-        console.log('Server listening...')
+                res.writeHead(404, { 'content-type': 'text/html' });
+                res.end(this.notFound);
+            }).listen(8089);
+            
+            console.log('Server listening...');
+        } catch (err) {
+            console.error('Error during server start:', err);
+        }
     }
 }
 
