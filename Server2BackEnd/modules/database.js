a88aeb8faea21e136432d40e4868514af697f840
tomhebcit <the18@my.bcit.ca>
2025-02-13 11:44:42 -0800

    fixed table generation script

--- Server2BackEnd/modules/database.js
+++ Server2BackEnd/modules/database.js
@@ -38,8 +38,7 @@ class Database {
       CREATE TABLE IF NOT EXISTS patients (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        age INT NOT NULL,
-        diagnosis TEXT
+        dateOfBirth DATE NOT NULL
       )
     `;
 
