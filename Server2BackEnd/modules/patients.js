dd74612080cf06e323f3c46f3ca14249c2b1eb74
tomhebcit <the18@my.bcit.ca>
2025-02-13 12:48:31 -0800

    made patient methods to be async as well

--- Server2BackEnd/modules/patients.js
+++ Server2BackEnd/modules/patients.js
@@ -1,24 +1,27 @@
-const url = require('url')
-const fs = require('fs')
+const url = require('url');
+const fs = require('fs');
 const Database = require('./database');
 
 class Patients {
 
   constructor() {
-    this.dictionary = []
-    this.reqCount = 0
-    this.messages = JSON.parse(fs.readFileSync('./lang/en/messages.json'))
+    this.dictionary = [];
+    this.reqCount = 0;
+    this.messages = JSON.parse(fs.readFileSync('./lang/en/messages.json'));
 
     this.adminDB = new Database(true); // Pass `true` to use the admin user
     this.userDB = new Database(false); // Use the default user
   }
 
+  // Async method to initialize both admin and user DBs
   async init() {
     try {
-      await this.adminDB.connect(); // Ensure DB connection is established
-      await this.adminDB.ensureTableExists(); // Ensure table is created if it doesn't exist
-      await this.adminDB.close(); // Close the connection
-      await this.userDB.connect(); // Ensure DB connection is established
+      // Wait for adminDB and userDB connections to be established
+      await this.adminDB.connect();
+      await this.adminDB.ensureTableExists();
+      await this.adminDB.close();
+      await this.userDB.connect();
+
       console.log('Patients class initialized successfully!');
     } catch (err) {
       console.error('Error during initialization:', err);
@@ -29,79 +32,79 @@ class Patients {
     this.userDB.close();
   }
 
-
-  handleRequest = (req, res) => {
+  async handleRequest(req, res) {
     if (req.method === "GET")
-      return this.handleGet(req, res)
+      return this.handleGet(req, res);
     if (req.method === "POST")
-      return this.handlePost(req, res)
+      return this.handlePost(req, res);
   }
 
-  handleGet = (req, res) => {
+  // Handle GET requests
+  async handleGet(req, res) {
     try {
-      const urlParts = url.parse(req.url)
-      const params = new URLSearchParams(urlParts.query)
-      const sqlQuery = params.get('sqlQuery')
-      const response = {}
+      const urlParts = url.parse(req.url);
+      const params = new URLSearchParams(urlParts.query);
+      const sqlQuery = params.get('sqlQuery');
+      const response = {};
 
       if (sqlQuery === null) {
-        response.error = this.messages.missingSqlQuery
-        return this.sendResponse(res, 404, JSON.stringify(response))
+        response.error = this.messages.missingSqlQuery;
+        return this.sendResponse(res, 404, JSON.stringify(response));
       }
 
-      this.init();
-      response = this.executeQuery(sqlQuery);
+      await this.init(); // Ensure DB is initialized before executing query
+      response.result = await this.executeQuery(sqlQuery); // Ensure query is awaited
       this.closeConnection();
-      return this.sendResponse(res, 200, JSON.stringify(response))
+      return this.sendResponse(res, 200, JSON.stringify(response));
     } catch (err) {
-      const response = {}
-      response.error = this.messages.serverError + err
-      return this.sendResponse(res, 500, JSON.stringify(response))
+      const response = {};
+      response.error = this.messages.serverError + err;
+      return this.sendResponse(res, 500, JSON.stringify(response));
     }
   }
 
-  handlePost = (req, res) => {
+  // Handle POST requests
+  async handlePost(req, res) {
     try {
-      const urlParts = url.parse(req.url)
-      const params = new URLSearchParams(urlParts.query)
-      const sqlQuery = params.get('sqlQuery')
-      const response = {}
+      const urlParts = url.parse(req.url);
+      const params = new URLSearchParams(urlParts.query);
+      const sqlQuery = params.get('sqlQuery');
+      const response = {};
 
       if (sqlQuery === null) {
-        response.error = this.messages.missingSqlQuery
-        return this.sendResponse(res, 404, JSON.stringify(response))
+        response.error = this.messages.missingSqlQuery;
+        return this.sendResponse(res, 404, JSON.stringify(response));
       }
 
-      this.init();
-      response = this.executeQuery(sqlQuery);
+      await this.init(); // Ensure DB is initialized before executing query
+      response.result = await this.executeQuery(sqlQuery); // Ensure query is awaited
       this.closeConnection();
-      
-      return this.sendResponse(res, 200, JSON.stringify(response))
+      return this.sendResponse(res, 200, JSON.stringify(response));
     } catch (err) {
-      const response = {}
-      response.error = this.messages.serverError + err
-      return this.sendResponse(res, 500, JSON.stringify(response))
+      const response = {};
+      response.error = this.messages.serverError + err;
+      return this.sendResponse(res, 500, JSON.stringify(response));
     }
   }
 
-  sendResponse = (res, status, message) => {
+  sendResponse(res, status, message) {
     res.writeHead(status, {
       'access-control-allow-methods': 'GET, POST',
       'Access-Control-Allow-Origin': '*',
       'content-type': "application/json"
-    })
-    res.write(message)
-    res.end()
+    });
+    res.write(message);
+    res.end();
   }
 
-  executeQuery = (sqlQuery) => {
+  async executeQuery(sqlQuery) {
+    // You can handle querying logic here, this can be an async method
     for (let i = 0; i < this.dictionary.length; i++) {
       if (this.dictionary[i].sqlQuery === sqlQuery)
-        return this.dictionary[i]
+        return this.dictionary[i];
     }
-    return null
+    return null;
   }
-
 }
 
-module.exports = Patients
+module.exports = Patients;
